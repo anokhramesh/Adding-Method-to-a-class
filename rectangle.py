f7100edf4f7204b3a11d5148d580c6395142b16d
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-24 12:19:02 +0400

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,16 @@
+# Adding methods to a class
+class Rectangle: 
+    pass
+rect1=Rectangle()
+rect2=Rectangle()
+
+rect1.height = 20
+rect2.height = 30
+
+rect1.width = 40
+rect2.width = 10
+
+# print Area of rect1
+print("Area of rect1 is=",rect1.height*rect1.width)
+# print Area of rect2
+print("Area of rect2 is =",rect2.height*rect2.width)
